1f41487a4536c7031d8392e2aac2eb89eadb5a89
sylwia.szlacan <sylwia.szlacan@intive.com>
2021-10-07 14:34:49 +0200

    Initial commit - SpringBoot project template

---
+++ src/main/java/com/test/loganalyzer/LogAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.test.loganalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LogAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAnalyzerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/test/loganalyzer/LogAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.loganalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
